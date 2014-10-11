3494fc2e498cc319fe24cc245ead8edae582188c
Steven Sheldon <steven@sasheldon.com>
2014-10-11 01:30:04 -0700

    Initial documentation.

---
+++ implementors/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rope'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/0.12.0/core/cmp/trait.PartialOrd.html' title='core::cmp::PartialOrd'>PartialOrd</a> for <a class='struct' href='rope/struct.Rope.html' title='rope::Rope'>Rope</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/iter/trait.Iterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['rope'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;'a&gt; <a class='trait' href='http://doc.rust-lang.org/0.12.0/core/iter/trait.Iterator.html' title='core::iter::Iterator'>Iterator</a>&lt;&amp;'a <a href='http://doc.rust-lang.org/0.12.0/std/primitive.str.html'>str</a>&gt; for <a class='struct' href='rope/struct.RopeStrings.html' title='rope::RopeStrings'>RopeStrings</a>&lt;'a&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
